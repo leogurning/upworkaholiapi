86ef0a0cc716405082a63222c921a5c76b6e1331
leo gurning <leogurning@gmail.com>
2019-07-03 23:36:54 +0700

    Init api project files #3

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  serverport: 1000,
+  database: 'mongodb+srv://guestApp:IniGu3st01@cluster0-ctv4w.mongodb.net/baseapp?ssl=true&authSource=admin&retryWrites=true&w=majority',
+  // database: 'mongodb+srv://guest:IniGuest01@lenocluster0-s2nns.mongodb.net/aftDb?ssl=true&authSource=admin&retryWrites=true',
+  jwt_secret: '!$3cret4@uth',
+};
